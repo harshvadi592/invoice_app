68b3a87c20516e347fd543e8f340dd64643bea80
harshvadi07 <harshvadi12345@gmail.com>
2024-12-27 22:14:20 +0530

    Remove previously tracked files and add .gitignore

--- core/lib/src/navigation/app_router.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-abstract class AppRouter {
-  Route<dynamic>? toRoute(Widget page, RouteSettings? settings) {
-    return MaterialPageRoute(builder: (_) => page, settings: settings);
-  }
-
-  Route<dynamic>? handleRoute(RouteSettings settings);
-}
--- features/feature_home/lib/src/navigation/home_feature_navigation_config.dart
+++
@@ -1,7 +0,0 @@
-import 'package:core/core.dart';
-
-class HomeFeatureNavigationConfig extends FeatureNavigationConfig {
-  static const name = 'home_feature_navigation_config';
-
-  const HomeFeatureNavigationConfig() : super(id: name);
-}
