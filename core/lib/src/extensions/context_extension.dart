380db0cd27487ce35a914346a94f8f51679643f0
harshvadi07 <harshvadi12345@gmail.com>
2024-12-27 22:13:02 +0530

    Remove previously tracked files and add .gitignore

--- core/lib/src/extensions/context_extension.dart
+++
@@ -1,9 +0,0 @@
-import 'package:core/core.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-extension BuildContextExtension on BuildContext {
-  AppColorScheme get colorStyling {
-    return Provider.of<AppColorScheme>(this);
-  }
-}
--- features/feature_home/lib/feature_home.dart
+++
@@ -1,2 +0,0 @@
-export 'src/di/home_feature_dependency_resolver.dart';
-export 'src/navigation/home_feature_navigation_config.dart';
